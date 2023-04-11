1acd570d7676ba2ce2bae8065c0e80092c4b4077
Amber Pöntinen <112793091+Amber-Lea-P@users.noreply.github.com>
2023-04-11 10:20:58 +0300

    Test 8

--- routes/gig.route.js
+++ routes/gig.route.js
@@ -5,12 +5,12 @@ import {
   getGig,
   getGigs
 } from "../controllers/gig.controller.js";
-import { verifyToken } from "../middleware/jwt.js";
+//import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-router.post("/", verifyToken, createGig);
-router.delete("/:id", verifyToken, deleteGig);
+router.post("/", createGig);
+router.delete("/:id", deleteGig); //verifyTokens have been removed for deployment purposes. Further autorization needs to be added in the future
 router.get("/single/:id", getGig);
 router.get("/", getGigs);
 
