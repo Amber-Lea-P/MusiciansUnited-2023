12788d817b030fbc8f730863b83bd2df00101022
Amber Pöntinen <112793091+Amber-Lea-P@users.noreply.github.com>
2023-04-11 10:15:00 +0300

    test 3

--- routes/order.route.js
+++ routes/order.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { verifyToken } from "../middleware/jwt.js";
+//import { verifyToken } from "../middleware/jwt.js";
 import { getOrders, intent, confirm } from "../controllers/order.controller.js";
 const router = express.Router();
 
 //router.post("/:gigId", verifyToken, createOrder );
 router.get("/", getOrders );
-router.post("/create-payment-intent/:id", verifyToken, intent);
-router.put("/", verifyToken, confirm);
+router.post("/create-payment-intent/:id",  intent); //verifyToken removed only in test deployment environtment
+router.put("/", confirm); //verifyToken removed only in test deployment environtment
 export default router;
