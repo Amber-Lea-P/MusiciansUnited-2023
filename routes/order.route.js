6b7bff9f6fdc98ad481cdf49eb0059c5a6aa655e
Amber Pöntinen <112793091+Amber-Lea-P@users.noreply.github.com>
2023-04-11 10:00:57 +0300

    test2

--- routes/order.route.js
+++ routes/order.route.js
@@ -4,7 +4,7 @@ import { getOrders, intent, confirm } from "../controllers/order.controller.js";
 const router = express.Router();
 
 //router.post("/:gigId", verifyToken, createOrder );
-router.get("/", verifyToken, getOrders );
+router.get("/", getOrders );
 router.post("/create-payment-intent/:id", verifyToken, intent);
 router.put("/", verifyToken, confirm);
 export default router;
